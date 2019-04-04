304c4100a0dd5174a564fa7ad2bdaceac8c6f37f
Zahar Koval <ellesterate@gmail.com>
2019-04-05 01:23:11 +0300

    Added SIGINT handling

--- main.cpp
+++ main.cpp
@@ -1,69 +1,124 @@
 #include <unistd.h>
+#include <signal.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <cctype>
+#include <cassert>
 #include <cstring>
 
 using argv_t = std::vector<char*>;
 
-void tokenize(std::string& str,
+static volatile sig_atomic_t current_pid = 0;
+
+enum
+{
+    buffer_size = 255
+};
+
+template<typename ForwardIt>
+void tokenize(ForwardIt const& str_begin,
+              ForwardIt const& str_end,
               argv_t& result)
 {
-    std::string::iterator last_p = str.begin();
-
-    for (std::string::iterator pos;
-         last_p != str.end()
-      && (pos = std::find_if_not(last_p, str.end(), [](char c){ return std::isspace(c); })) != std::end(str); )
+    auto last_p = str_begin;
+    for (decltype(last_p) pos;
+         last_p != str_end
+      && (pos = std::find_if_not(last_p, str_end, [](char c){ return std::isspace(c) || c == 0; })) != str_end; )
     {
-        auto sec_iter = std::find_if(pos, str.end(), [](char c){ return std::isspace(c);});
-        if (sec_iter == str.end())
-            str.push_back('\0');
-        else
-            *sec_iter = '\0';
+        auto sec_iter = std::find_if(pos, str_end, [](char c){ return std::isspace(c) || c == 0; });
+        *sec_iter = '\0';
 
-        result.push_back(pos.base());
+        result.push_back(pos);
         last_p = sec_iter;
         ++last_p;
     }
 }
 
+void sigterm_handler(int)
+{
+    if (current_pid != 0)
+    {
+        kill(static_cast<pid_t>(current_pid), SIGINT);
+    }
+}
+
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
-    std::string buffer;
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = sigterm_handler;
+//    action.sa_flags = SA_RESTART;
+    sigaction(SIGINT, &action, nullptr);
+
+    char buff[buffer_size];
+    char* buff_end = buff;
     argv_t arg_list;
 
     while (true)
     {
-        buffer.clear();
+        auto const msg = ">>> ";
 
-        std::cout << ">>> ";
-        std::getline(std::cin, buffer);
-        if (buffer.empty()) { return 0; }
+        write(fileno(stdout), "\n", 1);
+        write(fileno(stdout), msg, strlen(msg));
+        auto cnt = read(fileno(stdin), buff, buffer_size - 1);
+        buff_end = buff + cnt;
+
+        if (errno != 0 && errno != EINTR)
+        {
+            fprintf(stderr, "%s\n", strerror(errno));
+            return -1;
+        }
+
+        if (cnt == 0 && errno != EINTR)
+            return 0;
+
+        errno = 0;
 
         arg_list.clear();
-        tokenize(buffer, arg_list);
+        *buff_end = '\0';
+        tokenize(static_cast<char*>(buff), buff_end, arg_list);
         arg_list.push_back(nullptr);
 
-        auto pid = fork();
-        if (pid == 0)
+        if (arg_list.size() < 2)
+            continue;
+
+        if (arg_list.size() == 2
+         && memcmp(arg_list[0], "exit", std::min(strlen(arg_list[0]), static_cast<size_t>(4))) == 0)
         {
-            return execve(arg_list[0], arg_list.data(), nullptr);
-        } else if (pid < 0)
+            return 0;
+        }
+
+        extern char** environ;
+
+        current_pid = fork();
+        if (current_pid == 0)
         {
-            std::cerr << strerror(errno) << std::endl;
+            return execve(arg_list[0], arg_list.data(), environ);
+        } else if (current_pid < 0)
+        {
+            fprintf(stderr, "%s\n", strerror(errno));
             return -1;
         } else
         {
             int status;
             auto pd = wait(&status);
+            current_pid = 0;
+            if (errno == EINTR)
+            {
+                assert(pd == -1);
+                errno = 0;
+                fputs("Interruped\n", stderr);
+                continue;
+            }
+
             if (pd == -1)
             {
-                std::cerr << strerror(errno) << std::endl;
+                fprintf(stderr, "%s\n", strerror(errno));
                 return -1;
             }
 
@@ -71,13 +126,13 @@ int main()
             {
                 auto exit_code = WEXITSTATUS(status);
                 if (exit_code)
-                    std::cerr << "Exited with code " << exit_code << std::endl;
+                    fprintf(stderr, "Exited with code %d\n", exit_code);
                 continue;
             }
 
             if (WIFSIGNALED(status))
             {
-                std::cerr << "Was killed by " << WTERMSIG(status) << std::endl;
+                fprintf(stderr, "Was killed by %s\n", strsignal(WTERMSIG(status)));
                 continue;
             }
         }
