b392fc07dd0fa9042d7a1d1f723e33746e011fed
Artem <artyom.ustinov99@yandex.ru>
2019-04-16 01:19:37 +0300

    added modification with envs

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@
 #include <string>
 #include <iterator>
 #include <wait.h>
+#include <map>
 
 // Created by Artem Ustinov at 15.04.2019
+
+std::map<std::string, std::string> envs;
 
 struct command {
     std::string path;
@@ -16,12 +19,18 @@ struct command {
     explicit command(const std::vector<std::string> &args) : path(args[0]), argv(args.size() + 1) {
         for (int i = 0; i < args.size(); i++) {
             argv[i] = new char[args[i].size()];
-            args[i].copy(argv[i], args[i].size(), 0);
+            args[i].copy(argv[i], args[i].size());
         }
         argv.back() = nullptr;
     }
 
-    command() = default;
+    command() : path(), argv{nullptr} {}
+
+    ~command() {
+        for (auto &i : argv) {
+            delete[]i;
+        }
+    }
 };
 
 
@@ -36,9 +45,20 @@ command parsecmd(const std::string &s) {
 
 }
 
+std::vector<char *> env_array() {
+    std::vector<char *> res;
+    for (auto &env:envs) {
+        auto tmp = env.first + "=" + env.second;
+        res.push_back(new char[tmp.size()]);
+        tmp.copy(res.back(), tmp.size());
+    }
+    res.push_back(nullptr);
+    return res;
+}
+
 int execve(command const &cmd) {
-    std::vector<char *> env(1, nullptr);
-    return execve(cmd.path.data(), cmd.argv.data(), env.data());
+    auto envsv = env_array();
+    return execve(cmd.path.data(), cmd.argv.data(), envsv.data());
 }
 
 void run(command const &cmd) {
@@ -56,18 +76,55 @@ void run(command const &cmd) {
         } else if (WIFEXITED(status)) {
             std::cout << "Returned code: " << WEXITSTATUS(status) << std::endl;
         } else if (WIFSIGNALED(status)) {
-            std::cerr << "Execution is stopped with signal " << WSTOPSIG(status) << std::endl;
+            std::cerr << "Execution is stopped with signal " << WTERMSIG(status) << std::endl;
         }
     }
 }
 
-int main(int argc, char *argv[], char *envp[]) {
+void add_envs(char *envp[]) {
+    int i = 0;
+    while (envp[i] != nullptr) {
+        std::string s = envp[i];
+        unsigned long pos = s.find('=');
+        envs[s.substr(0, pos)] = s.substr(pos + 1, s.size());
+        i++;
+    }
+}
+
+void add_env(const std::string &env) {
+    unsigned long pos = env.find('=');
+    if (pos == std::string::npos) {
+        std::cerr << "Invalid format of exporting variable " << env << std::endl;
+        return;
+    }
+    envs[env.substr(0, pos)] = env.substr(pos + 1, env.size());
+}
+
+
+void erase_env(const std::string &env) {
+    envs.erase(env);
+}
+
+int main(int argc, char **argv, char **envp) {
+    add_envs(envp);
     while (true) {
         std::string s;
         getline(std::cin, s);
         auto cmd = parsecmd(s);
         if (((cmd.path) == "exit") || (std::cin.eof())) {
             break;
+        }
+        if (cmd.path == "export" && cmd.argv.size() > 2) {
+            for (int i = 1; i < cmd.argv.size() - 1; i++) {
+                add_env(cmd.argv[i]);
+            }
+            continue;
+        }
+        if (cmd.path == "unset" && cmd.argv.size() > 2) {
+            for (int i = 1; i < cmd.argv.size() - 1; i++) {
+                erase_env(cmd.argv[i]);
+            }
+            continue;
         }
         run(cmd);
     }
